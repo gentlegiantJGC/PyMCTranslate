dfad226df88259603fb332ddf2a12bf04c315537
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2019-10-23 14:04:08 +0100

    TranslationManager.get_version now takes version or DataVersion input

--- PyMCTranslate/py3/translation_manager.py
+++ PyMCTranslate/py3/translation_manager.py
@@ -88,6 +88,13 @@ class TranslationManager:
 		"""
 		# Storage for each of the Version classes
 		self._versions: Dict[str, Dict[Tuple[int, int, int], 'Version']] = {}
+		self._version_remap: Dict[
+			Tuple[
+				str,
+				Union[Tuple[int, ...], int]
+			],
+			Tuple[int, int, int]
+		] = {}
 
 		# Create a class for each of the versions and store them
 		for version_name in directories(os.path.join(json_path, 'versions')):
@@ -95,6 +102,7 @@ class TranslationManager:
 				version = Version(os.path.join(json_path, 'versions', version_name), self)
 				self._versions.setdefault(version.platform, {})
 				self._versions[version.platform].setdefault(version.version_number, version)
+				self._version_remap[(version.platform, version.data_version)] = version.version_number
 
 	def platforms(self) -> List[str]:
 		"""
@@ -112,17 +120,66 @@ class TranslationManager:
 		assert platform in self._versions, f'The requested platform "{platform}" is not present'
 		return list(self._versions[platform].keys())
 
-	def get_version(self, platform: str, version_number: Tuple[int, int, int]) -> 'Version':
+	def get_version(self, platform: str, version_number: Union[int, Tuple[int, ...], List[int]]) -> 'Version':
 		"""
 		A method to get a Version class.
 		:param platform: The platform name (use TranslationManager.platforms to get the valid platforms)
-		:param version_number: The version number (use TranslationManager.version_numbers to get version numbers for a given platforms)
+		:param version_number: The version number or DataVersion (use TranslationManager.version_numbers to get version numbers for a given platforms)
 		:return: The Version class for the given inputs. Throws an AssertionError if it does not exist.
 		"""
-		assert platform in self._versions and version_number in self._versions[platform], f'The requested version "({platform}, {version_number})" is not present'
+		if platform == 'anvil':
+			platform = 'java'
+		if isinstance(version_number, list):
+			version_number = tuple(version_number)
+		assert platform in self._versions, f'The requested platform "({platform})" is not present'
+		if isinstance(version_number, int) or version_number not in self._versions[platform]:
+			version_number = self._get_version_number(platform, version_number)
 		return self._versions[platform][version_number]
 
-	def get_sub_version(self, platform: str, version_number: Tuple[int, int, int], force_blockstate=False) -> 'SubVersion':
+	def _get_version_number(self, platform: str, version_number: Union[int, Tuple[int, ...]]) -> Tuple[int, int, int]:
+		if (platform, version_number) not in self._version_remap:
+			if isinstance(version_number, int):
+				previous_version = None
+				next_version = None
+				for version_number_, version in self._versions[platform].items():
+					if version.data_version == version_number:
+						self._version_remap[(platform, version_number)] = version_number_
+						return version_number_
+					elif version.data_version > version_number:
+						if next_version is None:
+							next_version = version.data_version, version_number_
+						elif version.data_version < next_version[0]:
+							next_version = version.data_version, version_number_
+					else:
+						if previous_version is None:
+							previous_version = version.data_version, version_number_
+						elif version.data_version > previous_version[0]:
+							previous_version = version.data_version, version_number_
+				if next_version is not None:
+					self._version_remap[(platform, version_number)] = next_version[1]
+				elif previous_version is not None:
+					self._version_remap[(platform, version_number)] = previous_version[1]
+				else:
+					raise Exception(f'Could not find a version for DataVersion({platform}, {version_number})')
+
+			elif isinstance(version_number, tuple):
+				previous_version = None
+				for version_number_ in self._versions[platform].keys():
+					if version_number_ < version_number:
+						if previous_version is None:
+							previous_version = version_number_
+						elif version_number_ > previous_version:
+							previous_version = version_number_
+				if previous_version is not None:
+					self._version_remap[(platform, version_number)] = previous_version
+				else:
+					raise Exception(f'Could not find a version for Version({platform}, {version_number})')
+
+			else:
+				raise Exception(f'version number type {version_number.__class__} is not supported')
+		return self._version_remap[(platform, version_number)]
+
+	def get_sub_version(self, platform: str, version_number: Union[int, Tuple[int, ...], List[int]], force_blockstate=False) -> 'SubVersion':
 		"""
 		A method to get a SubVersion class.
 		:param platform: The platform name (use TranslationManager.platforms to get the valid platforms)
@@ -151,6 +208,7 @@ class Version:
 		self._platform = init_file['platform']
 		assert isinstance(init_file['version'], list) and len(init_file['version']) == 3, f'The version number defined in {version_path}/__init__.json is incorrectly formatted'
 		self._version_number = tuple(init_file['version'])
+		self._data_version: int = init_file.get('data_version', 0)
 		assert isinstance(init_file['block_format'], str)
 		self._block_format = init_file['block_format']
 		self._has_abstract_format = self._block_format in ['numerical', 'pseudo-numerical']
@@ -210,9 +268,17 @@ class Version:
 	def version_number(self) -> Tuple[int, int, int]:
 		"""
 		The version number of the version this class instance holds the data of.
-		Currently these are 'java', 'bedrock' and 'universal'
+		eg (1, 13, 2)
 		"""
 		return self._version_number
+
+	@property
+	def data_version(self) -> int:
+		"""
+		The DataVersion number of the version this class instance holds the data of.
+		This is an int but is only used for Java versions beyond 1.9 snapshot 15w32a. Other versions will default to 0.
+		"""
+		return self._data_version
 
 	def get(self, force_blockstate: bool = False) -> 'SubVersion':
 		"""
