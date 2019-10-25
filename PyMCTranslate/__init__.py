66bfe901fd7f194270c79b142affd94ab3be51d8
Podshot <ben.gothard3@gmail.com>
2019-10-24 20:37:51 -0400

    Added setup.py, empty __init__.py files for packaging, and updated basic_test.py

--- PyMCTranslate/__init__.py
+++ PyMCTranslate/__init__.py
@@ -3,7 +3,7 @@ from PyMCTranslate.py3.translation_manager import TranslationManager
 
 
 def new_translation_manager() -> TranslationManager:
-	"""Returns a new TranslationManager with the default files.
-	Each unique world should have a new TranslationManager because there is the
-	functionality to register custom (mod) blocks making each handler unique."""
-	return TranslationManager(os.path.join(os.path.dirname(__file__), 'json'))
+    """Returns a new TranslationManager with the default files.
+    Each unique world should have a new TranslationManager because there is the
+    functionality to register custom (mod) blocks making each handler unique."""
+    return TranslationManager(os.path.join(os.path.dirname(__file__), 'json'))
