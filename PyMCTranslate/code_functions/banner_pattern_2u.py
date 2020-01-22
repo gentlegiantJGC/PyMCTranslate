0167164d12c44adaf1c8d65c4c514a26a5884118
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-01-22 11:30:51 +0000

    Added better support for Banners

---
+++ PyMCTranslate/code_functions/banner_pattern_2u.py
@@ -0,0 +1,11 @@
+def main(nbt):
+    if nbt[0] == "compound" and "Patterns" in nbt[1] and nbt[1]["Patterns"][0] == "list":
+        return [
+            [
+                "", "compound",
+                [("utags", "compound"), ("Patterns", "list"), (index, "compound")],
+                "Color", ["int", 15 - pattern[1].get("Color")]]
+            for index, pattern in enumerate(nbt[1]["Patterns"][1]) if pattern[0] == "compound"
+        ]
+    else:
+        return []
---
+++ PyMCTranslate/code_functions/banner_pattern_fu.py
@@ -0,0 +1,13 @@
+def main(nbt):
+    if nbt[0] == "compound" and \
+            "utags" in nbt[1] and nbt[1]["utags"][0] == "compound" and \
+            "Patterns" in nbt[1]["utags"][1] and nbt[1]["utags"][1]["Patterns"][0] == "list":
+        return [
+            [
+                "", "compound",
+                [("Patterns", "list"), (index, "compound")],
+                "Color", ["int", 15 - pattern[1].get("Color")]]
+            for index, pattern in enumerate(nbt[1]["utags"][1]["Patterns"][1]) if pattern[0] == "compound"
+        ]
+    else:
+        return []
