b380e5de10bb60d9038fa93a1ae25e7990cba913
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-02-25 11:27:36 +0000

    Properties are now always NBT

--- PyMCTranslate/code_functions/bedrock_skull_rotation_2u.py
+++ PyMCTranslate/code_functions/bedrock_skull_rotation_2u.py
@@ -1,11 +1,7 @@
 def main(nbt):
     if nbt[0] == "compound" and "Rotation" in nbt[1] and nbt[1]["Rotation"][0] == "float":
         return {
-            "rotation": str(
-                int(
-                nbt[1]["Rotation"][1] // 22.5
-                ) % 16
-            )
+            "rotation": f"\"{int(nbt[1]['Rotation'][1] // 22.5) % 16}\""
         }
     else:
         return {
