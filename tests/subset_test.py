01449d54bb1ff3715ffb1ac00b2c8c8e85071c4f
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-04-07 16:55:45 +0100

    Extended subset test to check conversion from universal

--- tests/subset_test.py
+++ tests/subset_test.py
@@ -12,28 +12,50 @@ class SubSetTest(unittest.TestCase):
     def test_sub_set(self):
         translator = PyMCTranslate.new_translation_manager()
         universal_version = translator.get_version('universal', (1, 0, 0))
+        universal_blocks = universal_version.block
         for platform in translator.platforms():
             for version_number in translator.version_numbers(platform):
                 version = translator.get_version(platform, version_number)
                 blocks = version.block
                 for force_blockstate in [False, True] if version.has_abstract_format else [False]:
                     pymct_log.enable_console_log(force_blockstate)
-                    print(version, force_blockstate)
+                    print('To Universal', version, force_blockstate)
                     for namespace in blocks.namespaces(force_blockstate):
                         for base_name in blocks.base_names(namespace, force_blockstate):
                             for block in self._blockstates(
-                                version.block.get_specification(namespace, base_name, force_blockstate),
+                                blocks.get_specification(namespace, base_name, force_blockstate),
                                 namespace,
                                 base_name
                             ):
-                                universal_obj = version.block.to_universal(block, force_blockstate=force_blockstate)[0]
+                                universal_obj = blocks.to_universal(block, force_blockstate=force_blockstate)[0]
                                 if isinstance(universal_obj, Block):
+                                    if force_blockstate:
+                                        self.assertEqual(universal_obj.namespace, 'universal_minecraft', (version, force_blockstate, block, universal_obj))
                                     if universal_obj.namespace == 'universal_minecraft':
                                         self._is_sub_set(
                                             universal_obj,
                                             universal_version.block.get_specification(universal_obj.namespace, universal_obj.base_name),
-                                            (version, force_blockstate)
+                                            (version, force_blockstate, block)
                                         )
+
+                    print('From Universal', version, force_blockstate)
+                    for namespace in universal_blocks.namespaces():
+                        for base_name in universal_blocks.base_names(namespace):
+                            for block in self._blockstates(
+                                universal_blocks.get_specification(namespace, base_name),
+                                namespace,
+                                base_name
+                            ):
+                                version_obj = blocks.from_universal(block, force_blockstate=force_blockstate)[0]
+                                if isinstance(version_obj, Block):
+                                    self.assertEqual(version_obj.namespace, 'minecraft', (version, force_blockstate, block, version_obj))
+                                    self._is_sub_set(
+                                        version_obj,
+                                        blocks.get_specification(version_obj.namespace, version_obj.base_name, force_blockstate=force_blockstate),
+                                        (version, force_blockstate, block)
+                                    )
+
+
 
     def _is_sub_set(self, obj: Block, spec: dict, info: Tuple[Any, ...]):
         spec_properties = spec.get('properties', {})
