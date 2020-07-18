f9dd1effc8cc50f7ae51025fe715e57fb34b8fd8
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-07-18 14:38:22 +0100

    Attempt to build with intersphinx

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -44,7 +44,7 @@ extensions = [
     "sphinx.ext.autodoc",
     # "sphinx_autodoc_typehints",
     "sphinx.ext.viewcode",
-    # "sphinx.ext.intersphinx",
+    "sphinx.ext.intersphinx",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
