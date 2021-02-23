c5cedc1c9df55e8bf8a090c7454bf6451497c1cc
gentlegiantJGC <james_clare1@yahoo.co.uk>
2021-02-23 14:16:19 +0000

    Reformatted

--- PyMCTranslate/__pyinstaller/hook-PyMCTranslate.code_functions.py
+++ PyMCTranslate/__pyinstaller/hook-PyMCTranslate.code_functions.py
@@ -3,7 +3,11 @@ import pkgutil
 import PyMCTranslate.code_functions
 
 hiddenimports = [
-    name for _, name, _ in pkgutil.walk_packages(PyMCTranslate.code_functions.__path__, PyMCTranslate.code_functions.__name__ + ".")
+    name
+    for _, name, _ in pkgutil.walk_packages(
+        PyMCTranslate.code_functions.__path__,
+        PyMCTranslate.code_functions.__name__ + ".",
+    )
 ]
 
 datas = collect_data_files("PyMCTranslate", excludes=["__pyinstaller"])
