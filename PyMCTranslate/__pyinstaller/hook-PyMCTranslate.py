39805bd3721a46c5ff4fd966e2b6e21361888333
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-01-11 17:19:57 +0000

    Fixed the pyinstaller hook to include all the files
    Previously this was done in the amulet map editor spec file

--- PyMCTranslate/__pyinstaller/hook-PyMCTranslate.py
+++ PyMCTranslate/__pyinstaller/hook-PyMCTranslate.py
@@ -1,6 +1,10 @@
-from PyInstaller.utils.hooks import collect_data_files
+import glob
+import os.path
+import re
+from PyInstaller.utils.hooks import get_package_paths
 import pkgutil
 import PyMCTranslate.code_functions
+
 
 hiddenimports = [
     name
@@ -9,3 +13,36 @@ hiddenimports = [
         PyMCTranslate.code_functions.__name__ + ".",
     )
 ]
+
+
+def get_glob_files(g):
+    """Get all files matching the given glob."""
+    return [p for p in glob.glob(g, recursive=True) if os.path.isfile(p)]
+
+
+def collect_data_files(package, excludes=()):
+    """
+    The function that comes with pyinstaller takes a long time.
+
+    :param package: The package name to find files in.
+    :param excludes: A list of regexes to exclude.
+    :return: A list of files.
+    """
+    root, package_path = get_package_paths(package)
+    # get all files
+    d = get_glob_files(os.path.join(package_path, "**", "*"))
+    # remove the files that match the regexes
+    for ex in excludes:
+        match = re.compile(ex)
+        d = [p for p in d if not match.fullmatch(p)]
+    return [(p, os.path.dirname(os.path.relpath(p, root))) for p in d]
+
+
+datas = collect_data_files(
+    "PyMCTranslate",
+    excludes=[
+        r"^.+\.(py[cod]?|dll|so|dylib|log|md)$",
+        re.escape(os.sep).join([".*PyMCTranslate", "json", r".*\.json"]),
+    ],
+)
+print("datas", datas[-5:])
