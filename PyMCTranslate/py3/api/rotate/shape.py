52b8d2f751edf3a13cdc745768949bb3ff5770b9
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-02-23 14:31:49 +0000

    Added jigsaw rotation

--- PyMCTranslate/py3/api/rotate/shape.py
+++ PyMCTranslate/py3/api/rotate/shape.py
@@ -14,13 +14,12 @@ class RailShape(BaseVectorBlockShape):
         (TAG_String("ascending_south"),): [(0, -1, -1), (0, 1, 1)],
         (TAG_String("ascending_west"),): [(1, -1, 0), (-1, 1, 0)],
         (TAG_String("ascending_east"),): [(-1, -1, 0), (1, 1, 0)],
-
         (TAG_String("east_west"),): [(1, 0, 0), (-1, 0, 0)],
         (TAG_String("north_south"),): [(0, 0, 1), (0, 0, -1)],
     }
 
     def is_valid(
-            self, namespace: str, base_name: str, specification: BlockSpecification
+        self, namespace: str, base_name: str, specification: BlockSpecification
     ) -> bool:
         return set(specification.valid_properties.get("shape", ())) == {
             TAG_String("ascending_east"),
@@ -41,10 +40,8 @@ class RailShapePlus(BaseVectorBlockShape):
         (TAG_String("ascending_south"),): [(0, -1, -1), (0, 1, 1)],
         (TAG_String("ascending_west"),): [(1, -1, 0), (-1, 1, 0)],
         (TAG_String("ascending_east"),): [(-1, -1, 0), (1, 1, 0)],
-
         (TAG_String("east_west"),): [(1, 0, 0), (-1, 0, 0)],
         (TAG_String("north_south"),): [(0, 0, 1), (0, 0, -1)],
-
         (TAG_String("north_east"),): (1, 0, -1),
         (TAG_String("north_west"),): (-1, 0, -1),
         (TAG_String("south_east"),): (1, 0, 1),
@@ -52,7 +49,7 @@ class RailShapePlus(BaseVectorBlockShape):
     }
 
     def is_valid(
-            self, namespace: str, base_name: str, specification: BlockSpecification
+        self, namespace: str, base_name: str, specification: BlockSpecification
     ) -> bool:
         return set(specification.valid_properties.get("shape", ())) == {
             # ascending
@@ -67,5 +64,42 @@ class RailShapePlus(BaseVectorBlockShape):
             TAG_String("north_east"),
             TAG_String("north_west"),
             TAG_String("south_east"),
-            TAG_String("south_west")
+            TAG_String("south_west"),
+        }
+
+
+@BlockShapes.register
+class JigsawShape(BaseVectorBlockShape):
+    Properties = ("orientation",)
+    Vectors = {
+        (TAG_String("down_north"),): (0, -2, -1),
+        (TAG_String("down_east"),): (1, -2, 0),
+        (TAG_String("down_south"),): (0, -2, 1),
+        (TAG_String("down_west"),): (-1, -2, 0),
+        (TAG_String("north_up"),): (0, 0, -2),
+        (TAG_String("east_up"),): (2, 0, 0),
+        (TAG_String("south_up"),): (0, 0, 2),
+        (TAG_String("west_up"),): (-2, 0, 0),
+        (TAG_String("up_north"),): (0, 2, -1),
+        (TAG_String("up_east"),): (1, 2, 0),
+        (TAG_String("up_south"),): (0, 2, 1),
+        (TAG_String("up_west"),): (-1, 2, 0),
+    }
+
+    def is_valid(
+        self, namespace: str, base_name: str, specification: BlockSpecification
+    ) -> bool:
+        return set(specification.valid_properties.get("orientation", ())) == {
+            TAG_String("down_north"),
+            TAG_String("down_east"),
+            TAG_String("down_south"),
+            TAG_String("down_west"),
+            TAG_String("north_up"),
+            TAG_String("east_up"),
+            TAG_String("south_up"),
+            TAG_String("west_up"),
+            TAG_String("up_north"),
+            TAG_String("up_east"),
+            TAG_String("up_south"),
+            TAG_String("up_west"),
         }
