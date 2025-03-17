c2848dff1875dc74ddbdf82b367aef27a6000260
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-03-17 08:39:10 +0000

    Updated versioneer

--- PyMCTranslate/__init__.py
+++ PyMCTranslate/__init__.py
@@ -8,7 +8,6 @@ from .py3 import (
     build_number,
 )
 
-from ._version import get_versions
+from . import _version
 
-__version__ = get_versions()["version"]
-del get_versions
+__version__ = _version.get_versions()["version"]
